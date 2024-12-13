a49cc39289036397713ea235981d6840a50f534e
Brunna Vict�ria da Silva Vicente <56012087861@>
2024-12-13 15:48:17 -0300

    first commit

---
+++ e-commerce vite/ecommerce/scripts/database.js
@@ -0,0 +1,338 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Anel Solitário Prata - You & Me ", 
+        preco: 900.00, 
+        descricao:"Anel Color em Prata 925 com Rodolita, Algumas vezes confundida com rubi, a gema rodolita é uma variedade da granada com uma coloração atrativa que vai do pink ao roxo. Seu nome deriva do grego rhodon (rosa) e lithos (pedra). ",
+        imagemProduto: {
+            img1:"./images/produto1.jpg",
+            img2:"./images/produto1.jpg",
+            img3:"./images/produto1.jpg",
+            img4:"./images/produto1.jpg",
+
+        },
+        categoriaProduto: 'Aneis',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Colar de ouro - Flor de Jasmin", 
+        preco: 1220.00, 
+        descricao:"O Colar de Ouro Flor de jamin da Victoria's tem como inspiração a beleza da natureza. A joia inclui um aglomerado de pedras, nas lapidações gota e navete, em torno de uma pedra central redonda e que compõem as pétalas e folhas. Uma releitura elegante das formas que encontramos na natureza. ",
+        imagemProduto: {
+            img1:"./images/produto2.png",
+            img2:"./images/produto2.png",
+            img3:"./images/produto2.png",
+            img4:"../images/produto2.png",
+    },
+        categoriaProduto: 'Colares',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Anel de Ouro rose - Shine", 
+        preco: 1100.00, 
+        descricao:"Faça todos os seus sonhos se realizarem com este delicado Anel Shine Em Ouro Rose, recoberto com zircônia cúbica em um aro finalizado em ouro rosê 14K. Simples e elegante, este anel em Y pode ser usado sozinho, como uma peça de destaque brilhante, ou combinado com outros anéis combináveis para criar um look exclusivo, só seu..",
+        imagemProduto: {
+            img1:"./images/produto3.jpg",
+            img2:"./images/produto3.jpg",
+            img3:"./images/produto3.jpg",
+            img4:"./images/produto3.jpg",
+
+        },
+        categoriaProduto: 'Aneis',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"Brinco de prata - Lovers", 
+        preco: 800.00, 
+        descricao:"Um par de brincos elegantes para dar de presente para seu grande amor, cada um dos brincos de argola de coração com halo brilhante ostenta um pendente de zircônia cúbica rosa em formato de coração circundados por um halo pavé. O pavé da argola é destacado por uma zircônia cúbica redonda e transparente. Combine com um colar ou um anel para um toque colorido em qualquer ocasião.",
+        imagemProduto: {
+            img1:"./images/brincos.png",
+            img2:"./images/brincos.png",
+            img3:"./images/brincos.png",
+            img4:"./images/brincos.png",
+
+        },
+        categoriaProduto: 'Brincos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:5,
+        tituloProduto:"Trono de Ferro Game Of Thrones", 
+        preco: 480.00, 
+        descricao:"O Pingente Trono de Ferro é uma homenagem ao icônico símbolo do poder de GOT. Este detalhado acessório captura a majestade e a intriga de Westeros. Uma joia de prata que remete a busca pela liderança. Um toque de elegância e mistério para qualquer coleção de charms.",
+        imagemProduto: {
+            img1:"./images/tronor.png",
+            img2:"./images/tronor.png",
+            img3:"./images/tronor.png",
+            img4:"./images/tronor.png",
+
+        },
+        categoriaProduto: 'Pulseiras',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:6,
+        tituloProduto:"Anel de Ouro GOT - Coroa Casa do Dragão", 
+        preco: 800.00, 
+        descricao:"O Anel de Ouro Coroa Casa do Dragão é um símbolo de poder e sofisticação. Inspirado em Game of Thrones, é a joia finalizada a ouro 14k perfeita para aqueles que desejam conquistar o trono de ferro e reinar com elegância. Faça parte da herança Targaryen. Disponível em diferentes tamanhos! Nota: Todos os acessórios Pandora Shine são banhados a ouro",
+        imagemProduto: {
+            img1:"./images/coroar.png",
+            img2:"./images/coroar.png",
+            img3:"./images/coroar.png",
+            img4:"./images/coroar.png",
+
+        },
+        categoriaProduto: 'Aneis',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:7,
+        tituloProduto:"Charm Pendente GOT - Espada Agulha de Arya Stark", 
+        preco: 300.00, 
+        descricao:"Mostre sua lealdade com o Charm Pendente Espada Agulha de a Guerra dos Tronos. Esta peça em dois tons é fabricada em prata e revestida a ouro 14k, com a forma da icônica Agulha de Arya Stark, que ela recebe do seu meio-irmão Jon Snow. Inclui elementos decorativos para um toque adicional de mistério e poder e também apresenta a gravação NOT TODAY. Uma peça essencial para quem adora a Casa Stark de GOT. Use-a sozinha, para se lembrar da sua força interior e resiliência, ou conjugue-a com as suas outras peças preferidas da Guerra dos Tronos.",
+        imagemProduto: {
+            img1:"./images/espadar.png",
+            img2:"./images/espadar.png",
+            img3:"./images/espadar.png",
+            img4:"./images/espadar.png",
+
+        },
+        categoriaProduto: 'Colares',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:8,
+        tituloProduto:"Charm Ouro Pendente GOT - Fogo do Dragão", 
+        preco: 700.00, 
+        descricao:"O Pingente de Ouro Fogo do Dragão é uma joia finalizada a ouro 14k, em homenagem à Casa Targaryen de GOT. Com detalhes intrincados, pedra vermelha e um design que evoca a majestade medieval, este charm pendente é uma adição a qualquer pulseira. Use-o para mostrar sua fidelidade ao mundo de Westeros ou como um símbolo de força e coragem! Nota: Todos os acessórios Pandora Shine são banhados a ouro.",
+        imagemProduto: {
+            img1:"./images/dragaor.png",
+            img2:"./images/dragaor.png",
+            img3:"./images/dragaor.png",
+            img4:"./images/dragaor.png",
+
+        },
+        categoriaProduto: 'Pulseiras',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:9,
+        tituloProduto:"Charm de Ouro Pendente Disney Maçã da Branca de Neve", 
+        preco: 559.50, 
+        descricao:"Mergulhe no mundo mágico da Disney com o Charm de Ouro Pendente Maçã Envenenada da Branca de Neve. Uma peça exclusiva da coleção Disney x Pandora. Este bereloque banhado a ouro 14k captura a icônica maçã em vidro murano vermelho, um símbolo atemporal dos contos de fadas. Adicione um toque de nostalgia e mistério à sua pulseira e celebre a icônica vilã Rainha Má. Perfeito para colecionadores e fãs da Disney que buscam peças únicas e cheias de significados.",
+        imagemProduto: {
+            img1:"./images/macabrancare.png",
+            img2:"./images/macabrancare.png",
+            img3:"./images/macabrancare.png",
+            img4:"./images/macabrancare.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+    {
+        codigoProduto:10,
+        tituloProduto:"Charm Disney Carruagem Brilhante Da Cinderela - Victoria's Rose", 
+        preco: 884.50, 
+        descricao:"Mergulhe no mundo mágico da Disney com o Charm Pendente Carruagem Brilhante da Cinderela. Uma peça encantadora da coleção Disney x Victoria's. Este berloque, finalizado em uma sofisticada liga metálica com ouro rose 14k, captura a elegância da icônica carruagem da Cinderela, adornada com brilhantes pedras de zircônia. Um símbolo de magia e sonhos realizados, perfeito para adicionar um toque de encanto à sua pulseira. Ideal para colecionadores e fãs da Disney que valorizam peças exclusivas e repletas de significados.",
+        imagemProduto: {
+            img1:"./images/carruagemre.png",
+            img2:"./images/carruagemre.png",
+            img3:"./images/carruagemre.png",
+            img4:"./images/carruagemre.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+    {
+        codigoProduto:11,
+        tituloProduto:"Charm Prata Pendente Disney Pixar - Wall-E e Eve Brilha no Escuro", 
+        preco: 756.50, 
+        descricao:"Celebre o amor de outro mundo com o Charm Wall-E e Eve, que Brilha no Escuro, da coleção Disney Pixar x Victoria's. Este berloque em prata de lei e coração vazado pendente mostra a gentileza de Wall-E, que oferece à Eve sua preciosa planta. Um segundo pingnte pendente de coração plano traz a mensagem Love you more than words can say (Amo mais que palavras podem dizer) e ilumina seu mundo com uma lua em esmalte azul que brilha no escuro. Adicione este charm à sua coleção, ou leve a quem você daria a lua de presente.",
+        imagemProduto: {
+            img1:"./images/wallere.png",
+            img2:"./images/wallere.png",
+            img3:"./images/wallere.png",
+            img4:"./images/wallere.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+    {
+        codigoProduto:12,
+        tituloProduto:"Charm Prata Pendente Disney Timao E Pumba Divisivel", 
+        preco: 644.50, 
+        descricao:" Celebre a amizade inesquecível de Timão e Pumba com o Charm Prata Pendente inspirado no clássico filme da Disney, O Rei Leão. Feito em prata esterlina, este pingente é dividido em duas partes, representando os dois personagens icônicos. Cada parte é ricamente detalhada com esmalte em tons de rosa, vinho, preto, bege e marrom, capturando perfeitamente a personalidade vibrante e diversificada de Timão e Pumba. Use este charm como uma lembrança dos momentos divertidos e emocionantes do filme, ou como uma declaração de amizade fiel em sua Pulseira Pandora e do seu melhor amigo.",
+        imagemProduto: {
+            img1:"./images/timaoepumbare.png",
+            img2:"./images/timaoepumbare.png",
+            img3:"./images/timaoepumbare.png",
+            img4:"./images/timaoepumbare.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+    {
+        codigoProduto:13,
+        tituloProduto:"Anel de Prata Aros Cruzados em Pavé", 
+        preco: 1889.00, 
+        descricao:"Torne-se bidimensional com o Anel de Prata Dois Aros Cruzados em Pavé da coleção Pandora Timeless. Pedras brilhantes cobrem a frente e as laterais de ambas as faixas cruzadas, que têm larguras ligeiramente diferentes. A faixa interna é a mais larga e apresenta o icônico logotipo Pandora gravado no interior. O presente perfeito para quem adora empilhar joias!",
+        imagemProduto: {
+            img1:"./images/anelaros.png",
+            img2:"./images/anelaros.png",
+            img3:"./images/anelaros.png",
+            img4:"./images/anelaros.png",
+
+        },
+        categoriaProduto: 'Aneis',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+   
+    {
+        codigoProduto:14,
+        tituloProduto:"Brinco De Argola Concha Do Mar - Pandora Ocean", 
+        preco: 790.00, 
+        descricao:"Brinco De Argola Concha Do Mar - Pandora Ocean. Inspirado nos tesouros das profundezas, esses brincos de argola adicionam um pouco de elegância do oceano a qualquer roupa. O design apresenta um toque deslumbrante em dois tons de Prata de Lei e Pandora Rose ™. Use os brincos de concha para adicionar um clima de praia à sua roupa ou para mostrar ao mundo que, apesar de estar calmo na superfície, seus sentimentos são profundos, como o oceano.",
+        imagemProduto: {
+            img1:"./images/brincoconcha.png",
+            img2:"./images/brincoconcha.png",
+            img3:"./images/brincoconcha.png",
+            img4:"./images/brincoconcha.png",
+
+        },
+        categoriaProduto: 'Brincos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:15,
+        tituloProduto:"Anel Solitário Prata - You & Me ", 
+        preco: 900.00, 
+        descricao:"Brinco De Argola Concha Do Mar - Pandora Ocean. Inspirado nos tesouros das profundezas, esses brincos de argola adicionam um pouco de elegância do oceano a qualquer roupa. O design apresenta um toque deslumbrante em dois tons de Prata de Lei e Pandora Rose ™. Use os brincos de concha para adicionar um clima de praia à sua roupa ou para mostrar ao mundo que, apesar de estar calmo na superfície, seus sentimentos são profundos, como o oceano.",
+        imagemProduto: {
+            img1:"./images/brincoconcha.png",
+            img2:"./images/brincoconcha.png",
+            img3:"./images/brincoconcha.png",
+            img4:"./images/brincoconcha.png",
+
+        },
+        categoriaProduto: 'Brincos',
+        classificacaoProduto:"Novidades",
+        exibirHome: true
+    },
+    {
+        codigoProduto:16,
+        tituloProduto:"Charm Prata GOT - Daenerys Targaryen Mãe de Dragões", 
+        preco: 1230.00, 
+        descricao:"O Charm Daenerys é uma homenagem à Mãe dos Dragões de GOT, um símbolo de coragem e determinação. Este pingente rico em detalhes captura a força interior da casa Targaryen. Adicione esta joia de prata e esmalte vermelho à sua pulseira para lembrar-se da busca por justiça!",
+        imagemProduto: {
+            img1:"./images/maedosdragoes.png",
+            img2:"./images/maedosdragoes.png",
+            img3:"./images/maedosdragoes.png",
+            img4:"./images/maedosdragoes.png",
+
+        },
+        categoriaProduto: 'Pulseiras',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:17,
+        tituloProduto:"Brinco de Prata GOT - Dragão Curvado", 
+        preco: 899.00, 
+        descricao:"O Brinco Dragão Curvado de Game Of Thrones é uma fusão de beleza e força. Inspirados na Casa Targaryen, essa joia de prata lembra os grandes protetores de Westeros. Ricos em detalhes, são ideais para os fãs da série e apreciadores de peças únicas. Um toque de estilo que exala determinação e sofisticação.",
+        imagemProduto: {
+            img1:"./images/brincosdragoes.png",
+            img2:"./images/brincosdragoes.png",
+            img3:"./images/brincosdragoes.png",
+            img4:"./images/brincosdragoes.png",
+
+        },
+        categoriaProduto: 'Brincos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:18,
+        tituloProduto:"Brinco de Prata GOT - Dragão Curvado", 
+        preco: 899.00, 
+        descricao:"O Brinco Dragão Curvado de Game Of Thrones é uma fusão de beleza e força. Inspirados na Casa Targaryen, essa joia de prata lembra os grandes protetores de Westeros. Ricos em detalhes, são ideais para os fãs da série e apreciadores de peças únicas. Um toque de estilo que exala determinação e sofisticação.",
+        imagemProduto: {
+            img1:"./images/brincosdragoes.png",
+            img2:"./images/brincosdragoes.png",
+            img3:"./images/brincosdragoes.png",
+            img4:"./images/brincosdragoes.png",
+
+        },
+        categoriaProduto: 'Brincos',
+        classificacaoProduto:"Mais_Vendidos",
+        exibirHome: true
+    },
+    {
+        codigoProduto:19,
+        tituloProduto:"Charm Prata Pendente Disney Pixar - Remy de Ratatouille", 
+        preco: 562.00, 
+        descricao:"Siga seus sonhos com o Remy da Pixar. Este charm pendente em prata de lei ostenta o Remy da Disney e Ratatouille da Pixar, usando o seu chapéu de chef e fazendo o gesto de beijo de um chef. O esmalte rosa aplicado à mão dá vida às suas orelhas e ao seu nariz e o esmalte preto confere o brilho da vida aos seus olhos. Combine este charm em suas joias Pandora como um lembrete para que siga seu coração e nunca desista.",
+        imagemProduto: {
+            img1:"./images/rato.png",
+            img2:"./images/rato.png",
+            img3:"./images/rato.png",
+            img4:"./images/rato.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+    {
+        codigoProduto:20,
+        tituloProduto:"Charm Prata Pendente Disney Pixar - Remy de Ratatouille", 
+        preco: 562.00, 
+        descricao:"Siga seus sonhos com o Remy da Pixar. Este charm pendente em prata de lei ostenta o Remy da Disney e Ratatouille da Pixar, usando o seu chapéu de chef e fazendo o gesto de beijo de um chef. O esmalte rosa aplicado à mão dá vida às suas orelhas e ao seu nariz e o esmalte preto confere o brilho da vida aos seus olhos. Combine este charm em suas joias Pandora como um lembrete para que siga seu coração e nunca desista.",
+        imagemProduto: {
+            img1:"./images/carta.png",
+            img2:"./images/carta.png",
+            img3:"./images/carta.png",
+            img4:"./images/carta.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+    {
+        codigoProduto:21,
+        tituloProduto:"Charm Prata Pendente Disney Pixar - Remy de Ratatouille", 
+        preco: 562.00, 
+        descricao:"Siga seus sonhos com o Remy da Pixar. Este charm pendente em prata de lei ostenta o Remy da Disney e Ratatouille da Pixar, usando o seu chapéu de chef e fazendo o gesto de beijo de um chef. O esmalte rosa aplicado à mão dá vida às suas orelhas e ao seu nariz e o esmalte preto confere o brilho da vida aos seus olhos. Combine este charm em suas joias Pandora como um lembrete para que siga seu coração e nunca desista.",
+        imagemProduto: {
+            img1:"./images/carta.png",
+            img2:"./images/carta.png",
+            img3:"./images/carta.png",
+            img4:"./images/carta.png",
+
+        },
+        categoriaProduto: 'Disney',
+        classificacaoProduto:"Promocoes",
+        exibirHome: true    
+    },
+]
